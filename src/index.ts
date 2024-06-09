3b59cb0fb0b599b60b083e5842567e631de2793d
Sonal Singh <sonal1201kumar@gmail.com>
2024-06-09 12:58:42 +0530

    Basic api setup done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express"
+
 import serverConfig from "./config/server.config";
+import apiRouter from "./routes";
 
 const app = express();
 
+app.use('/api',apiRouter)
+
 app.listen(serverConfig.PORT,()=>{
-    console.log(`Listening to portt ${serverConfig.PORT}`);
+    console.log(`Listening to port ${serverConfig.PORT}`);
 })
